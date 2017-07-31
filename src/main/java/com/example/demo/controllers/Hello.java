6dbe336ade1e06a5e5016e814c236a6da20e63ff
唐菊 <635465547@qq.com>
2017-07-31 11:09:13 +0800

    post  get  方法，获取url中的变量

--- src/main/java/com/example/demo/controllers/Hello.java
+++ src/main/java/com/example/demo/controllers/Hello.java
@@ -1,16 +1,29 @@
 package com.example.demo.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by tj on 2017/7/4.
  */
 
 @RestController
+@RequestMapping("/")
 public class Hello {
-    @RequestMapping("/")
+    //RequestMapping  可在浏览器中用url获取
+    @RequestMapping("home")
     public String value(){
         return "Hello wrold";
+    }
+
+    //PostMapping 只可通过post方法获取
+    @PostMapping("message/{messageId}")
+    public String postMessage(@PathVariable("messageId") String messageId){
+        return messageId;
+    }
+
+    //GetMapping 只可通过get方法获取
+    @GetMapping("message/{messageId}")
+    public String getMessage(@PathVariable("messageId") String messageId){
+        return messageId;
     }
 }
